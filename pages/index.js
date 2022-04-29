e7806d6f6d3580d23e9d476c593088abecdb5480
myeonghyeonjeong <jngmnghn@colosseum.kr>
2022-04-29 12:53:48 +0900

    prettier 적용

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <>Home</>
-  )
+  return <>Home</>;
 }

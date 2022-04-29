6d5ea5b2739fdc167a379de45391e1738cd55d67
myeonghyeonjeong <jngmnghn@colosseum.kr>
2022-04-29 12:58:35 +0900

    emotion 패키지 설치, 테마 파일추가, normalize 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
 
+import { ThemeProvider } from "@emotion/react";
+import theme from "styles/theme";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
---
+++ styles/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    white: "#fff",
+    primary: "#083a90",
+    gray: "#f4f5f7",
+  },
+};
+
+export default theme;

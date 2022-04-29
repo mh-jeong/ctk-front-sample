1c580a4a39dcee48d04f7c8c1adc29a0b4928d33
myeonghyeonjeong <jngmnghn@colosseum.kr>
2022-04-29 13:00:31 +0900

    title 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import "../styles/globals.css";
 
 import { ThemeProvider } from "@emotion/react";
+import Head from "next/head";
 import theme from "styles/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>COLO - Global</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 

b8b8530d4c4974db0c14e8061b7e775b4e1662ca
myeonghyeonjeong <jngmnghn@colosseum.kr>
2022-04-29 12:55:06 +0900

    next.config.js 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.resolve = {
+      alias: {
+        components: path.resolve(__dirname, "/components"),
+        hooks: path.resolve(__dirname, "/hooks"),
+        pages: path.resolve(__dirname, "/pages"),
+        mobx: path.resolve(__dirname, "/mobx"),
+        styles: path.resolve(__dirname, "/styles"),
+        ...config.resolve.alias,
+      },
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
